d8dd7e19ff464ef0d73c389b57c9b91274d78fec
Edore <edore@ualberta.ca>
2026-03-11 11:40:29 -0600

    Renamed SRC folder to Source Code to avoid any Compiler errors

---
+++ Source Code/app/src/main/java/com/example/willow_lotto_app/OrganizerDashboardActivity.java
@@ -0,0 +1,22 @@
+package com.example.willow_lotto_app;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OrganizerDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_organizer_dashboard);
+
+        Button backButton = findViewById(R.id.backToProfileButton);
+        backButton.setOnClickListener(v -> finish());
+    }
+
+
+
+
+}
