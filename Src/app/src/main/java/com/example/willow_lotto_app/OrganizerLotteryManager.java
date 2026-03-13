abd7a4f88f06aeaf98a57764fadfc77d45a4958b
Edore <edore@ualberta.ca>
2026-03-13 13:53:46 -0600

    Implemented testing for Notifications. Updated Gradle to include Mockito

--- Src/app/src/main/java/com/example/willow_lotto_app/OrganizerLotteryManager.java
+++ Src/app/src/main/java/com/example/willow_lotto_app/OrganizerLotteryManager.java
@@ -1,15 +1,22 @@
 package com.example.willow_lotto_app;
 
+import android.util.Log;
+import android.widget.Toast;
+
 import com.google.firebase.Timestamp;
-import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
+
+
 public class OrganizerLotteryManager {
+
 
     public interface LotteryCallback {
         void onSuccess(String message, List<Registration> affectedRegistrations);
@@ -24,6 +31,7 @@ public class OrganizerLotteryManager {
     private final FirebaseFirestore db;
     private final RegistrationStore registrationRepository;
     private final NotificationStore notificationRepository;
+    private static final String TAG = "OrganizerLotteryManager";
 
     public OrganizerLotteryManager() {
         this.db = FirebaseFirestore.getInstance();
@@ -275,6 +283,7 @@ public class OrganizerLotteryManager {
                     "lottery_invited"
             );
 
+
             notificationRepository.sendNotificationToUser(
                     registration.getUserId(),
                     notification,
@@ -285,6 +294,7 @@ public class OrganizerLotteryManager {
                             if (completed[0] == total) {
                                 callback.onSuccess("Lottery draw completed successfully.", selected);
                             }
+                            System.out.print("Notification Sent");
                         }
 
                         @Override
@@ -313,6 +323,7 @@ public class OrganizerLotteryManager {
                         List<Registration> result = new ArrayList<>();
                         result.add(replacement);
                         callback.onSuccess("Replacement entrant selected successfully.", result);
+                        System.out.print("Notification Sent replacement");
                     }
 
                     @Override
